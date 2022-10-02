7c2dbec4729c48f577202ffab3579bf6ed50e85b
Agamyrat Chariyev <agamyrat.chariyev@gmail.com>
2022-10-02 09:42:42 +0500

    git sync db added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const path = require('path');
+const { sequelize } = require('./db/models');
 
 dotenv.config({ path: path.join(__dirname, './config/.env') });
 
@@ -8,3 +9,11 @@ const PORT = process.env.PORT;
 app.listen(process.env.PORT, () => {
   console.log(`Server is running ${PORT}`);
 });
+
+try {
+  sequelize.authenticate(() => {
+    console.log('\nDatabase is OK');
+  });
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
